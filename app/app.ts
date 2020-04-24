3031336f63d2d28c2d1a46d5d1569fe31a5ff519
wan.yixin <962717593@qq.com>
2020-04-24 14:23:03 +0800

    '修改项目结构'

--- app/app.ts
+++ app/app.ts
@@ -13,7 +13,7 @@ var app = express();
 app.use(res_api);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'app/views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use('/', loaderjs);
 
 
