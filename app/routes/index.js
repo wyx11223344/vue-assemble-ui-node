b5c0179af0daed6a08663ab31a22fb756cafafaf
wan.yixin <962717593@qq.com>
2020-04-24 11:00:57 +0800

    '更改位置'

---
+++ app/routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var templateHTML = require('../template/defaultIframe');
+router.get('/', function (req, res, next) {
+    res.render('index');
+});
+router.get('/index.html', function (req, res, next) {
+    res.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Expires': new Date().toUTCString()
+    });
+    res.write(templateHTML.startHTML);
+    res.write('测试\n');
+    res.end(templateHTML.endHTML);
+});
+module.exports = router;
+//# sourceMappingURL=index.js.map
