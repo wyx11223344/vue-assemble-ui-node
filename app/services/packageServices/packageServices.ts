82531b5da8d939f819d37748675a1804d99aee3e
wan.yixin <962717593@qq.com>
2020-07-06 17:26:31 +0800

    '添加缺少的注释'

--- app/services/packageServices/packageServices.ts
+++ app/services/packageServices/packageServices.ts
@@ -3,14 +3,30 @@ import NpmPublish from '../../models/npmPublish';
 import NpmPublishMapper from '../../mapper/npmPublishMapper';
 
 export default class PackageServices implements PackageServicesImp{
+
+    /**
+     * 通过id查询npm详细信息(,分割查询多个)
+     * @param {String} npmId npm包的id
+     * @returns {Promise<any>}
+     */
     async getNpmById(npmId: string): Promise<NpmPublish[]> {
         return NpmPublishMapper.getNpmById(npmId);
     }
 
+    /**
+     * 通过name查询npm详细信息(,分割查询多个)
+     * @param {String} npmName npm包的名称
+     * @returns {Promise<any>}
+     */
     async getNpmByName(npmName: string): Promise<NpmPublish[]> {
         return NpmPublishMapper.getNpmByName(npmName);
     }
 
+    /**
+     * 更新或新增npm包
+     * @param {NpmPublish} npmPublish 新增包对象
+     * @returns {Promise<boolean>}
+     */
     async setNpm(npmPublish: NpmPublish): Promise<boolean> {
         return new Promise(async (resolve) => {
             if (npmPublish.id) {
@@ -20,7 +36,6 @@ export default class PackageServices implements PackageServicesImp{
                 if (npmObj[0] && npmObj[0].version >= npmPublish.version) {
                     resolve(false);
                 } else {
-                    console.log(123);
                     await NpmPublishMapper.updateNpm(npmPublish);
                     resolve(true);
                 }
